e6595190aa7b4dc5064f667ad78903a1b794e300
Brendon Guedes <lordbguedes@gmail.com>
2020-04-15 23:26:56 -0300

    useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/rocket.svg';
 import astronaut from './assets/astronaut.svg';
 import './App.css';
 
 function App() {
+  const [techs, setTechs] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('techs');
+
+    if (storageTech) {
+      setTechs(JSON.parse(storageTech));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
   return (
-    <div className="App">
-      <img className="logo" src={logo} alt="Rocketseat Logo" />
-      <h2>Welcome to OmniStack!</h2>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-      <img className="astronaut" src={astronaut} alt="Astronaut" />
-    </div>
+    <>
+      <div className="App">
+        <img className="logo" src={logo} alt="Rocketseat Logo" />
+        <h2>My name is Brendon Guedes</h2>
+        <h3>Estou aprendendo Hooks</h3>
+        <ul>
+          {techs.map(t => (
+            <li key={t}> {t}</li>
+          ))}
+        </ul>
+        <p>
+          <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+        </p>
+        <button type="button" onClick={handleAdd}>
+          Add new tech
+        </button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+        <img className="astronaut" src={astronaut} alt="Astronaut" />
+      </div>
+    </>
   );
 }
 
